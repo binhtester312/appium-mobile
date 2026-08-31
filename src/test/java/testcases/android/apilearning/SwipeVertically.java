3da7252ecaae47a574851fbfb9c39f5406c60558
binhtester312 <binhtester312@users.noreply.github.com>
2026-08-31 21:22:40 +0700

    feat(android): add TestNG Assert verification for 'You found me!' robot element in SwipeVertically

--- src/test/java/testcases/android/apilearning/SwipeVertically.java
+++ src/test/java/testcases/android/apilearning/SwipeVertically.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -32,8 +33,7 @@ public class SwipeVertically {
         swipeLabel.click();
 
         // Make sure I'm on the target screen (Swipe module screen)
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//*[@text='Swipe horizontal' or @content-desc='Swipe-screen' or @text='Swipe']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@content-desc='Swipe']")));
 
         // Get the mobile screen sizes
         Dimension windowSize = appiumDriver.manage().window().getSize();
@@ -46,7 +46,16 @@ public class SwipeVertically {
         int yStartPoint = 90 * screenHeight / 100;
         int yEndPoint = 10 * screenHeight / 100;
 
-        // Perform W3C Touch Actions (Appium 8/9 standard migration from TouchAction)
+        // Perform W3C Touch Actions
+
+        // Thứ tự Logic chuẩn
+        // Thao tác 1 (swipeUp) - Vuốt từ dưới (90%) lên trên (10%):
+        // Cuộn màn hình xuống dưới để xem các nội dung bên dưới (như icon con robot
+        // "You found me!").
+        // Nghỉ 3 giây: Để bạn quan sát rõ màn hình đã cuộn xuống.
+        // Thao tác 2 (swipeDown) - Vuốt từ trên (10%) xuống dưới (90%):
+        // Cuộn màn hình ngược trở lại về vị trí ban đầu trên đỉnh trang.
+        // Nghỉ 3 giây: Để bạn quan sát màn hình đã cuộn lại về đầu trang.
 
         // --- 1. SCROLL DOWN PAGE (Swipe from bottom 90% to top 10%) ---
         System.out.println(">>> 1. Executing SWIPE UP (Scrolling page down to bottom)...");
@@ -61,13 +70,21 @@ public class SwipeVertically {
         swipeUp.addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
 
         // B3: Di chuyển ngón tay lên phía mép trên (10% height) trong 1s
-        swipeUp.addAction(finger1.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), xEndPoint, yEndPoint));
+        swipeUp.addAction(finger1.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), xEndPoint,
+                yEndPoint));
 
         // B4: Nhấc ngón tay lên
         swipeUp.addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
         // B5: Gửi chuỗi hành động cho Appium thực thi
         appiumDriver.perform(Collections.singletonList(swipeUp));
+
+        // --- ASSERTION: Kiểm tra đã nhìn thấy icon/text "You found me!" sau khi cuộn xuống ---
+        WebElement robotIconElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[contains(@text, 'You found me') or contains(@content-desc, 'You found me') or contains(@text, 'found me')]")
+        ));
+        Assert.assertTrue(robotIconElement.isDisplayed(), "Robot icon / 'You found me!' text is not displayed!");
+        System.out.println(">>> ASSERTION PASSED: Found 'You found me!' robot element on screen!");
 
         // Dừng 3 giây để bạn nhìn rõ màn hình đã cuộn xuống!
         try {
@@ -86,7 +103,8 @@ public class SwipeVertically {
 
         swipeDown.addAction(finger2.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
 
-        swipeDown.addAction(finger2.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), xStartPoint, yStartPoint));
+        swipeDown.addAction(finger2.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(),
+                xStartPoint, yStartPoint));
 
         swipeDown.addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
